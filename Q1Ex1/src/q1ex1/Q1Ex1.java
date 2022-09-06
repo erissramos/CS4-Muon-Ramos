bb896b74744dae3ae680b6099c5a11a45df5a5ca
erissramos <70785301+erissramos@users.noreply.github.com>
2022-09-06 09:03:44 +0800

    Add files via upload

---
+++ Q1Ex1/src/q1ex1/Q1Ex1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1ex1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Classmate 1");
+        String cName1 = "Aaron"; 
+        String favSubject1 = "Math";
+        String cAge1 = "16";
+        System.out.printf("Name: %s %n Favorite Subject: %s %n Age: %s", cName1, favSubject1, cAge1);
+       
+        
+        System.out.println("Classmate 2");
+        String cName2 = "Patrick";
+        String favSubject2 = "Physics";
+        String cAge2 = "15";
+        System.out.printf("Name: %s %n Favorite Subject: %s %n Age: %s", cName2, favSubject2, cAge2);
+        
+        System.out.println("Classmate 3");
+        String cName3 = "Rael";
+        String favSubject3 = "Chemistry";
+        String cAge3 = "16";
+        System.out.printf("Name: %s %n Favorite Subject: %s %n Age: %s", cName3, favSubject3, cAge3);
+
+       
+        
+    }
+    
+}
