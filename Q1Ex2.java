80937dea0b6f90893ea4ccca7c0969058bafa070
erissramos <70785301+erissramos@users.noreply.github.com>
2022-09-15 11:29:38 +0800

    Add files via upload

--- Q1Ex2.java
+++ Q1Ex2.java
@@ -18,64 +18,50 @@ public class Q1Ex2 {
      */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Welcome to Higher or Lower! What will you do?");
-        System.out.println("- Start game");
-        System.out.println("- Change settings");
-        System.out.println("- End application");
-        System.out.println("\nYou will... ");
-        String response = sc.nextLine();
+        String input ;
+        int range = 10;
+        int min = 1;
+        int guesses = 3;
+        do {
+            System.out.println("Welcome to Higher or Lower! What will you do?");
+            System.out.println("- Start game");
+            System.out.println("- Change settings");
+            System.out.println("- End application");
+            System.out.println("\nYou will... ");
 
-        int random = (int) Math.floor(Math.random()*10)+1;
-        int guess = 5;
-        int answer = -1;
-        
-        boolean start = (response.equalsIgnoreCase("Start game"));
-        boolean options = (response.equalsIgnoreCase("Change settings"));
-        boolean exit = (response.equalsIgnoreCase("End application"));
-        
-        boolean playAgain = true;
-        
-        if (start == true) {
-            while (answer != random) {
-                System.out.printf("You have %d guess(es). What is your guess?", guess);
-                answer = sc.nextInt();
-                System.out.println(random);
-
-                if (answer == random) {
-                    System.out.println("You got it!");
+            input = sc.nextLine();
+            switch(input) {
+                case "Start game":
+                    String repeat;
+                    do {
+                    int random = (int) Math.floor(Math.random()*range)+min;
+                    for(int remaining = guesses; remaining > 0; remaining--) {
+                        System.out.printf("You have %d guess(es) left. What will you do?", remaining);
+                        int answer = Integer.parseInt(sc.nextLine());
+                        if (answer == random) {
+                            System.out.println("Congratulations! You win!");
+                            break;
+                        } else if (answer < random) {
+                            System.out.printf("Guess higher! You have %d guess(es) left. What will you do?", remaining);
+                        } else if (answer > random) {
+                            System.out.printf("Guess lower! You have %d guess(es) left. What will you do?", remaining);
+                        } else if (answer != random) {
+                            System.out.println("You lose...");
+                            break;
+                        }
+                    }         
+                    
+                    System.out.println("Do you want to play again? (y/n)");
+                    repeat = sc.nextLine();
+                    } while(repeat.equals("y"));
                     break;
-                } else if (answer < random) {
-                    guess--;
-                    System.out.printf("Guess higher! You have %d guess(es) left. What is your guess?", guess);
-                } else if (answer > random) {
-                    guess--;
-                    System.out.printf("Guess lower! You have %d guess(es) left. What is your guess?", guess);
-                } else if (answer!=random) {
-                    System.out.println("You lose...");
-                }
             }
-        } else if (options == true) {
-            System.out.println("What is the lower limit of the random number?");
-            String lower = sc.nextLine();
-            System.out.println("What is the upper limit of the random number?");
-            String upper = sc.nextLine();
-            System.out.println("How many guesses are allowed?");
-            String guessNo = sc.nextLine();
-            System.out.printf("\nLower limit: %s", lower);
-            System.out.printf("\nUpper limit: %s", upper);
-            System.out.printf("\nGuesses: %s", guessNo);
-
-        } else if (exit == true) {
-            System.out.println("Thank you for playing");
-            return;
-        }
-        System.out.println("Play again? (y/n)");
-        String repeat = sc.nextLine();
-        if (repeat.equalsIgnoreCase("\ny")) {
-            playAgain = true;
-        } else if (repeat.equalsIgnoreCase("\nn")) {
-            playAgain = false;
-        }
+        } while(!input.equalsIgnoreCase("End application"));
+        
+        
+        
+       
+        
     }
+    
 }
